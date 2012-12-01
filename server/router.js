984df943fd68be9ee7c33668e4421a0dd70e2cf6
neoziro <neoziro@gmail.com>
2012-12-01 17:53:24 +0100

    gitignore

--- server/router.js
+++ server/router.js
@@ -1,6 +1,32 @@
+var neo4j = require("neo4j"),
+db = new neo4j.GraphDatabase("http://localhost:7474");
+
 var router = function(app) {
-  app.get('/', function(req, res){
+  app.get('/', function(req, res) {
     res.render("app");
+  });
+  
+  app.get("/test/", function(req, res) {
+    
+    var query = [
+      'START user=node({userId})',
+      'MATCH (user) -[:likes]-> (other)',
+      'RETURN other'
+    ].join('\n');
+
+    var params = {
+      userId: 5
+    };
+    
+    
+
+    db.query(query, params, function (err, results) {
+      if (err) {
+        throw err;
+      }
+      res.end(results);
+    });
+    
   });
 };
 
